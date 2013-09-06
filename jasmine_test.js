434fa4cf1a20555d47dd153da94730e87742990a
juanantonioruz <juanantonioruz@gmail.com>
2013-09-06 09:57:09 +0200

    all things ok

--- jasmine_test.js
+++ jasmine_test.js
@@ -1,10 +1,12 @@
 // var ey=require("./node2");
 // console.log(ey);
 var my_cljs_lib=require("./src/cljs-demo/hello");
-console.log(my_cljs_lib);
-//my_cljs_lib.example.hello();
+//console.log(my_cljs_lib);
+my_cljs_lib.example.hello();
+my_cljs_lib.cljs_demo.hello.examplecallback();
 
-var other=require("./node2");
-console.log(other);
+// this way don't work!
+//var other=require("./node2");
+//console.log(other);
 //other.example.hello("asda");
 
